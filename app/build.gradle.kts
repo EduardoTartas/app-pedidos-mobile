49513c53765cdec499cdad5cf2c6058b41419956
Luis Felipe Lopes <luis.felipe.lopes1275@gmail.com>
2026-04-17 19:43:58 -0400

    build(gradle): Ajustando dependências após merge da develop na branch atual

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui-text-google-fonts:1.7.8")
+    implementation("dev.chrisbanes.haze:haze:1.1.1")
 
     // 3. ANDROID CORE & LIFECYCLE
     implementation(libs.androidx.core.ktx)
