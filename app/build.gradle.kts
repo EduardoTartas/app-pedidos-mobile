65a8eb644480060fcbd18278f474d66ac3192def
Luis Felipe Lopes <luis.felipe.lopes1275@gmail.com>
2026-04-17 21:47:07 -0400

    feat(HomePage): Tela inicial ajustada e implementada, contendo lógica de refresh, blur na bottom bar, e melhoradas lógicas de busca no banco das categorias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("dev.chrisbanes.haze:haze:0.6.2")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui-text-google-fonts:1.7.8")
--- app/src/main/java/dev/fslab/pedidos/ui/screens/HazeTest.kt
+++
@@ -1,9 +0,0 @@
-package dev.fslab.pedidos.ui.screens
-
-import dev.chrisbanes.haze.HazeState
-import dev.chrisbanes.haze.haze
-import dev.chrisbanes.haze.hazeChild
-
-fun testHaze() {
-    val h = HazeState()
-}
